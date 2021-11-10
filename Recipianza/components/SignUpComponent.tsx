0084b3473108a390c14ef400b796f8c91e44d608
Daksh <upadhyaydaksh96@gmail.com>
2021-11-10 04:17:35 -0500

    DU - Added more input fields & design changes.

--- Recipianza/components/SignUpComponent.tsx
+++ Recipianza/components/SignUpComponent.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 
 const SignUpComponent = () =>{
     return(
-        <SafeAreaView style = {styles.container}>
-            <View style={styles.topBox}>
-                <Image 
-                    source ={require('../assets/logo_boarding.png')} 
-                    style={{width: '100%', height: '100%'}}
-                />                
-            </View>
-            <View style = {styles.bottomBox}>
-                <TouchableOpacity style = {styles.buttonStyle}
-                    onPress={() => {
-                        alert("I am working")
-                    }}
-                >
-                <Text style = {styles.buttonFont}>Sign Up</Text>
-                </TouchableOpacity>
-            </View>
-
-        </SafeAreaView>
+            <SafeAreaView style = {styles.container}>
+                <View style={styles.topBox}>
+                    <Image 
+                        source ={require('../assets/logo_boarding.png')} 
+                        style={styles.logoBoarding}
+                    />                
+                </View>
+                <View style = {styles.bottomBox}>
+                <ScrollView>
+                    <View>
+                        <Text style={styles.signUpText}>Sign Up</Text>
+                    </View>
+                    <View style={styles.horizontalLine}/>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="First Name"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Last Name"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        keyboardType="email-address"
+                        textContentType="emailAddress"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        keyboardType="email-address"
+                        secureTextEntry={true}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Confirm 1 Password"
+                        keyboardType="email-address"
+                        secureTextEntry={true}
+                    />                    
+                    <TouchableOpacity style = {styles.buttonStyle}
+                        onPress={() => {
+                            alert("Sign Up")
+                        }}>
+                        <Text style = {styles.buttonFont}>Sign Up</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            alert("Login")
+                        }}>
+                        <Text style={styles.loginButton}>Already Have An Account? LOGIN</Text>
+                    </TouchableOpacity>
+                    </ScrollView>
+                </View>
+            </SafeAreaView>
     )
 }
 
@@ -30,24 +66,62 @@ const styles = StyleSheet.create({
         backgroundColor: 'black'
     },
     topBox:{
-        flex: 1.5,
-        justifyContent: "space-evenly",
-        alignItems: "center",
+        flex: 1.3,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
         padding: 15
+    },
+    logoBoarding:{
+        width: '100%', 
+        height: '100%'
     },
     bottomBox:{
         flex: 2,
         padding: 10,
-        backgroundColor: "#EED50F"
+        backgroundColor: '#EED50F',
+        borderTopRightRadius: 75
+    },
+    signUpText:{
+        marginTop: 30,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 28
+    },
+    horizontalLine:{
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        marginTop: 5,
+        marginLeft: 142,
+        marginRight: 142
+    },
+    input:{
+        height: 40,
+        margin: 12,
+        marginLeft: 30,
+        marginRight: 30,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor: '#FFFFFF'
     },
     buttonStyle:{
-        backgroundColor: "grey",
+        backgroundColor: '#FFFFFF',
+        margin: 12,
+        marginLeft: 30,
+        marginRight: 30,
         padding: 10,
-        alignItems: "center",
-        marginBottom: 2
+        borderWidth: 1,
+        alignItems: 'center',
+        marginBottom: 2,
+        borderRadius: 25
     },
     buttonFont:{
-        color: "white"
+        color: 'black',
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+    loginButton:{
+        marginTop: 20,
+        textAlign: 'center'
     }
 })
 
