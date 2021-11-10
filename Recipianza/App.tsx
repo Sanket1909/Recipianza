a8cccc0057f07ec29da3af5fd8f348734886c7e4
Daksh <upadhyaydaksh96@gmail.com>
2021-11-10 01:13:59 -0500

    Added Empty Sign Up Component
    DU - Added Empty Sign Up Component

--- Recipianza/App.tsx
+++ Recipianza/App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUpComponent from './components/SignUpComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Recipianza</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Recipianza</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <SignUpComponent/>
   );
 }
 
---
+++ Recipianza/components/SignUpComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native"
+
+const SignUpComponent = () =>{
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Sign Up Component</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default SignUpComponent;
