aded951a24add4b25d3bc44dbc05edbe9cb07610
Daksh <upadhyaydaksh96@gmail.com>
2021-11-10 05:09:11 -0500

    DU - Text Fields with Image Updated in Signup screen.

--- Recipianza/components/SignUpComponent.tsx
+++ Recipianza/components/SignUpComponent.tsx
@@ -11,54 +11,69 @@ const SignUpComponent = () =>{
                     />                
                 </View>
                 <View style = {styles.bottomBox}>
-                <ScrollView>
-                    <View>
-                        <Text style={styles.signUpText}>Sign Up</Text>
-                    </View>
-                    <View style={styles.horizontalLine}/>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="First Name"
-                        placeholderTextColor="#424242"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Last Name"
-                        placeholderTextColor="#424242"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Email"
-                        placeholderTextColor="#424242"
-                        keyboardType="email-address"
-                        textContentType="emailAddress"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Password"
-                        placeholderTextColor="#424242"
-                        keyboardType="email-address"
-                        secureTextEntry={true}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Confirm Password"
-                        placeholderTextColor="#424242"
-                        keyboardType="email-address"
-                        secureTextEntry={true}
-                    />                    
-                    <TouchableOpacity style = {styles.buttonStyle}
-                        onPress={() => {
-                            alert("Sign Up")
-                        }}>
-                        <Text style = {styles.buttonFont}>Sign Up</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => {
-                            alert("Login")
-                        }}>
-                        <Text style={styles.loginButton}>Already Have An Account? LOGIN</Text>
-                    </TouchableOpacity>
+                    <ScrollView style={styles.scrollView}>
+                        <View>
+                            <Text style={styles.signUpText}>Sign Up</Text>
+                        </View>
+                        <View style={styles.horizontalLine}/>
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/user.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="First Name"
+                                placeholderTextColor="#424242"
+                            />
+                        </View>
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/user.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="Last Name"
+                                placeholderTextColor="#424242"
+                            />
+                        </View>
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/email.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="Email"
+                                placeholderTextColor="#424242"
+                                keyboardType="email-address"
+                                textContentType="emailAddress"
+                            />
+                        </View>
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/password.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="Password"
+                                placeholderTextColor="#424242"
+                                keyboardType="email-address"
+                                secureTextEntry={true}
+                            />
+                        </View>
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/password.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="Confirm Password"
+                                placeholderTextColor="#424242"
+                                keyboardType="email-address"
+                                secureTextEntry={true}
+                            />
+                        </View>
+                        <TouchableOpacity style = {styles.buttonStyle}
+                            onPress={() => {
+                                alert("Sign Up")
+                            }}>
+                            <Text style = {styles.buttonFont}>Sign Up</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                alert("Login")
+                            }}>
+                            <Text style={styles.loginButton}>Already Have An Account? LOGIN</Text>
+                        </TouchableOpacity>
                     </ScrollView>
                 </View>
             </SafeAreaView>
@@ -86,8 +101,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#EED50F',
         borderTopRightRadius: 75
     },
+    scrollView:{
+        marginLeft: 15, 
+        marginRight: 15
+    },
     signUpText:{
-        marginTop: 30,
+        marginTop: 20,
         textAlign: 'center',
         fontWeight: 'bold',
         fontSize: 28
@@ -95,29 +114,43 @@ const styles = StyleSheet.create({
     horizontalLine:{
         borderBottomColor: 'black',
         borderBottomWidth: 2,
-        marginTop: 5,
+        marginTop: 10,
         marginLeft: 142,
-        marginRight: 142
+        marginRight: 142,
+        marginBottom: 20
     },
-    input:{
-        height: 40,
-        margin: 12,
-        marginLeft: 30,
-        marginRight: 30,
+    inputTextContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
         borderWidth: 1,
+        height: 50,
+        borderRadius: 5 ,
+        margin: 12
+    },
+    inputTextImage: {
         padding: 10,
-        backgroundColor: '#FFFFFF'
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode : 'stretch',
+        alignItems: 'center'
+    },
+    inputText:{
+        flex:1,
+        fontSize: 20
     },
     buttonStyle:{
         backgroundColor: '#FFFFFF',
-        margin: 12,
+        marginTop: 20,
         marginLeft: 30,
         marginRight: 30,
         padding: 10,
         borderWidth: 1,
         alignItems: 'center',
-        marginBottom: 2,
-        borderRadius: 25
+        borderRadius: 25,
+        height: 50
     },
     buttonFont:{
         color: 'black',
@@ -125,6 +158,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     loginButton:{
+        fontSize: 20,
         marginTop: 20,
         textAlign: 'center'
     }
