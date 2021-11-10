58e78b7ea53534d9e170625300d4f3f00a62af82
Daksh <upadhyaydaksh96@gmail.com>
2021-11-10 16:05:14 -0500

    DU - Added profile component for user.

---
+++ Recipianza/components/ProfileComponent.tsx
@@ -0,0 +1,139 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
+
+const Stack = createNativeStackNavigator();
+
+const ProfileComponent = ({navigation}: any) =>{
+    return(
+            <SafeAreaView style = {styles.container}>
+                <View style={styles.topBox}>
+                    <Image 
+                        source ={require('../assets/profile.png')}
+                    /> 
+                    <View style={styles.seperator}/>
+                    <View>
+                        <Text style={styles.signUpText}>Recipianza</Text>
+                    </View>
+                    <View style={styles.seperator}/>              
+                </View>
+                <View style = {styles.bottomBox}>
+                    <ScrollView style={styles.scrollView}>                        
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/user.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="First Name"
+                                placeholderTextColor="#424242"
+                            />
+                        </View>
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/user.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="Last Name"
+                                placeholderTextColor="#424242"
+                            />
+                        </View>
+                        <View style={styles.inputTextContainer}>
+                            <Image source={require('../assets/icons/email.png')} style={styles.inputTextImage} />
+                            <TextInput
+                                style={styles.inputText}
+                                placeholder="Email"
+                                placeholderTextColor="#424242"
+                                keyboardType="email-address"
+                                textContentType="emailAddress"
+                            />
+                        </View>                        
+                        <TouchableOpacity style = {styles.buttonStyle}
+                            onPress={() => {
+                                alert("Save")
+                            }}>
+                            <Text style = {styles.buttonFont}>Save</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style = {styles.buttonStyle}
+                            onPress={() => {
+                                alert("Logout")
+                            }}>
+                            <Text style = {[styles.buttonFont, styles.logoutText]}>Logout</Text>
+                        </TouchableOpacity>                    
+                    </ScrollView>
+                </View>
+            </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#EED50F'
+    },
+    topBox:{
+        flex: 1.5,
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    bottomBox:{
+        flex: 2,
+        padding: 10
+    },
+    scrollView:{
+        marginLeft: 15, 
+        marginRight: 15
+    },
+    signUpText:{
+        marginTop: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 28
+    },
+    seperator:{
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        padding: 10,
+        width: '100%'
+    },
+    inputTextContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        borderWidth: 1,
+        height: 50,
+        borderRadius: 5 ,
+        margin: 12
+    },
+    inputTextImage: {
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode : 'stretch',
+        alignItems: 'center'
+    },
+    inputText:{
+        flex:1,
+        fontSize: 20
+    },
+    buttonStyle:{
+        backgroundColor: '#FFFFFF',
+        marginTop: 20,
+        marginLeft: 30,
+        marginRight: 30,
+        padding: 10,
+        borderWidth: 1,
+        alignItems: 'center',
+        borderRadius: 25,
+        height: 50
+    },
+    buttonFont:{
+        color: 'black',
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+    logoutText:{
+        color: 'red'
+    }
+})
+
+export default ProfileComponent;
