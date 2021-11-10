600ec6c1b7cc15f46cf262a48a8adefae565fac9
Daksh <upadhyaydaksh96@gmail.com>
2021-11-10 17:56:11 -0500

    HM - Added Click on Recipe item.

--- Recipianza/components/HomeComponent.tsx
+++ Recipianza/components/HomeComponent.tsx
@@ -1,8 +1,9 @@
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import {FlatList, SafeAreaView, StyleSheet, View, Text, Image} from "react-native"
+import {FlatList, SafeAreaView, StyleSheet, View, Text, Image, TouchableOpacity} from "react-native"
+import RecipeDetailComponent from './RecipeDetailComponent';
 
-// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const recipeData = [
     {
@@ -17,38 +18,46 @@ const recipeData = [
         title: "SpongeBob SquarePants",
         preparationTime: '45 mins',
         description: "The Si mpsons description.", 
-        image: require("../assets/recipeImages/pizza.png"), 
+        image: require("../assets/recipeImages/pizza.png")
       }
     ] 
     
-    const renderItem = ({ item, index }: any) => (
-        <View style = {[styles.containerCell, { height: 160}]}>
-            <View style={{flexDirection: 'row', height: 120}}>
-                <View style={{flexDirection: 'column', flex: 1}}>
-                    <Text style={styles.title}> {item.title} </Text>
-                    <View style={styles.durationContainer}>
-                        <Image 
-                            source ={require('../assets/icons/time.png')} 
-                            style={styles.recipeDurationImage}
-                        /> 
-                        <Text style={styles.recipeDurationText}>{item.preparationTime}</Text>
+    const renderItem = ({ item, navigation }: any) => (
+        <TouchableOpacity
+            onPress={() => {
+                alert(JSON.stringify(item))
+                // alert(JSON.stringify(navigation))
+                navigation.navigate('RecipeDetail', {data: item})
+            }}
+        >
+            <View style = {[styles.containerCell, { height: 160}]}>
+                <View style={{flexDirection: 'row', height: 120}}>
+                    <View style={{flexDirection: 'column', flex: 1}}>
+                        <Text style={styles.title}> {item.title} </Text>
+                        <View style={styles.durationContainer}>
+                            <Image 
+                                source ={require('../assets/icons/time.png')} 
+                                style={styles.recipeDurationImage}
+                            /> 
+                            <Text style={styles.recipeDurationText}>{item.preparationTime}</Text>
+                        </View>
+                    </View>
+                    <View style={{flexDirection: 'column', flex: 1}}>
+                        <Image
+                            style={{ height: '100%', width: '100%'}}
+                            source={item.image}
+                            resizeMode="contain"
+                        />
                     </View>
                 </View>
-                <View style={{flexDirection: 'column', flex: 1}}>
-                    <Image
-                        style={{ height: '100%', width: '100%'}}
-                        source={item.image}
-                        resizeMode="contain"
-                    />
+                <View style={{flexDirection: 'row', height: 40}}>
+                    <Text> {item.description} </Text>
                 </View>
             </View>
-            <View style={{flexDirection: 'row', height: 40}}>
-                <Text> {item.description} </Text>
-            </View>
-        </View>
+        </TouchableOpacity>
     )
 
-const HomeComponent = ({navigation}: any) =>{
+const HomeComponentContent = ({navigation}: any) =>{
     return(
         <SafeAreaView style = {styles.container}>
             <View style={styles.container}>
@@ -61,6 +70,16 @@ const HomeComponent = ({navigation}: any) =>{
         </SafeAreaView>
     )
 }
+
+const HomeComponent =() => {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeComponentContent} options={{title: 'Welcome'}}/>
+            <Stack.Screen name="RecipeDetail" component={RecipeDetailComponent} options={{title: 'Detail'}}/>
+        </Stack.Navigator>
+    )
+} 
+
 
 const styles = StyleSheet.create({
     containerExp: {
@@ -100,17 +119,5 @@ const styles = StyleSheet.create({
     },
 
 })
-
-// const HomeComponent =() => {
-//     return(
-//         <Stack.Navigator>
-//             <Stack.Screen
-//                 name="Home"
-//                 component={HomeComponentContent}
-//                 options={{ title: 'Welcome' }}
-//             />
-//         </Stack.Navigator>
-//     )
-// } 
 
 export default HomeComponent;
